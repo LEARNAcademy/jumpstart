fbee45a5ec70c367beda51e529811609c65408d4
Charlean Baxter <charleanbaxter@charleans-mbp.attlocal.net>
2022-02-01 22:28:59 -0800

    added the dice theme per the vision of Elyse, Charlean, and Sarah

--- mini-jump/dice.js
+++ mini-jump/dice.js
@@ -1,38 +1,19 @@
-const diceRoll = () => {
+const rollDice = () => {
   var dice = Math.ceil(Math.random() * 6)
-
   if(dice === 1) {
-    return document.getElementById("output").innerHTML = "1"
+    document.getElementById("dice").innerHTML = dice
+    alert("Game over")
   } else if(dice === 2) {
-    return document.getElementById("output").innerHTML = "2"
+    document.getElementById("dice").innerHTML = dice
   } else if(dice === 3) {
-    return document.getElementById("output").innerHTML = "3"
+    document.getElementById("dice").innerHTML = dice
   } else if(dice === 4) {
-    return document.getElementById("output").innerHTML = "4"
+    document.getElementById("dice").innerHTML = dice
   } else if(dice === 5) {
-    return document.getElementById("output").innerHTML = "5"
+    document.getElementById("dice").innerHTML = dice
   } else if(dice === 6) {
-    return document.getElementById("output").innerHTML = "6"
+    document.getElementById("dice").innerHTML = "6 Extra Life"
   } else {
-    console.log("Dice fell off the table.")
+    alert("Ooops, the dice fell off the table!")
   }
 }
-
-
-// Step 1:
-// var dice = Math.ceil(Math.random() * 6)
-// if(dice === 1) {
-//   console.log("1")
-// } else if(dice === 2) {
-//   console.log("2")
-// } else if(dice === 3) {
-//   console.log("3")
-// } else if(dice === 4) {
-//   console.log("4")
-// } else if(dice === 5) {
-//   console.log("5")
-// } else if(dice === 6) {
-//   console.log("6")
-// } else {
-//   console.log("Dice fell off the table.")
-// }
